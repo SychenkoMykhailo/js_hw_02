bd4e97a5e58d7d12a7be723200fd99d98eae0401
SychenkoMykhailo <76732407+SychenkoMykhailo@users.noreply.github.com>
2021-01-19 12:55:41 +0200

    Update extratask-01.js

--- extratask-01.js
+++ extratask-01.js
@@ -7,3 +7,15 @@ const capitalize = function (string) {
 	console.log(NewArr.join(" "));
 }
 capitalize("the quick brown fox");
+
+// Через цикл:
+// const capitalize = function (string) {
+//   let arr = string.split(" ");
+//   let newarr = [];
+//   for (let elem of arr) {
+//     let word = elem[0].toUpperCase() + elem.slice(1);
+//     newarr.push(word);
+//   }
+//   console.log(newarr.join(" "));
+// };
+// capitalize("the quick brown fox");
